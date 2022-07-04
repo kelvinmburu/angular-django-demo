a8747917e2b4d8491a438d31ebea8cf398c16155
kelvinmburu <mburuhkelvin@gmail.com>
2022-07-04 12:14:03 +0300

    added files

---
+++ angapp/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Department, Employees
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = ('DepartmentId',
+                  'DepartmentName')
+        
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employees
+        fields = ('EmployeeId',
+                  'EmployeeName',
+                  'Department',
+                  'DateOfJoining',
+                  'PhotoFileName')
+        
